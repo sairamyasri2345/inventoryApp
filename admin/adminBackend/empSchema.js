dbc15be747fe8b00b426f6b9c225d32dc7ac852c
sairamyasri <112067104+sairamyasri2345@users.noreply.github.com>
2024-10-04 20:14:40 +0530

    Update empSchema.js

--- admin/adminBackend/empSchema.js
+++ admin/adminBackend/empSchema.js
@@ -7,7 +7,7 @@ const employeeSchema = new mongoose.Schema({
   designation: { type: String, required: true },
   department: { type: String, required: true },
   email: {type:String, required:true}, 
-  password: {type:String, required:true},
+ 
 });
 
 const Employee = mongoose.model('Employee', employeeSchema);
