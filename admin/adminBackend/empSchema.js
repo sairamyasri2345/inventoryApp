c9e5a423ddad4709d3243e1e26ee601cb86a12f0
sairamyasri <112067104+sairamyasri2345@users.noreply.github.com>
2024-10-04 20:22:31 +0530

    update code

--- admin/adminBackend/empSchema.js
+++ admin/adminBackend/empSchema.js
@@ -6,9 +6,8 @@ const employeeSchema = new mongoose.Schema({
   phoneNumber: { type: String, required: true },
   designation: { type: String, required: true },
   department: { type: String, required: true },
-  email: {type:String, required:true}, 
- 
+  email: { type: String, required: true }
 });
 
-const Employee = mongoose.model('Employee', employeeSchema);
-module.exports = Employee;
+mongoose.model('Employee', employeeSchema);
+
